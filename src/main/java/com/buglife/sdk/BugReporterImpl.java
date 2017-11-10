b2840fa39ae9f5a64b00a4ba89a927f9094a2f64
Alex Fu <alex.fu914@gmail.com>
2017-11-10 00:15:42 -0500

    Re-throw JSON exception and display error.

--- src/main/java/com/buglife/sdk/BugReporterImpl.java
+++ src/main/java/com/buglife/sdk/BugReporterImpl.java
@@ -23,12 +23,14 @@ import android.content.Context;
 import android.os.Build;
 import android.os.PersistableBundle;
 import android.support.annotation.RequiresApi;
+import android.widget.Toast;
 
 import com.buglife.sdk.reporting.BugReporter;
 import com.buglife.sdk.reporting.SubmitReportLegacyService;
 import com.buglife.sdk.reporting.SubmitReportService;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 final class BugReporterImpl implements BugReporter {
     private final Context mContext;
@@ -43,45 +45,44 @@ final class BugReporterImpl implements BugReporter {
     }
 
     @Override public void report(Report report) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            reportWithJobScheduler(report);
-        } else {
-            reportWithLegacy(report);
+        try {
+            JSONObject jsonReport = report.toJSON();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                reportWithJobScheduler(jsonReport);
+            } else {
+                reportWithLegacy(jsonReport);
+            }
+        } catch (JSONException e) {
+            Log.e("Failed to serialize bug report!", e);
+            Toast.makeText(mContext, R.string.error_serialize_report, Toast.LENGTH_LONG).show();
+            throw new RuntimeException(e);
         }
     }
 
-    private void reportWithLegacy(Report report) {
-        try {
-            String jsonReport = report.toJSON().toString();
-            SubmitReportLegacyService.start(mContext, jsonReport);
-        } catch (JSONException e) {
-            Log.e("Failed to encode report!", e);
-        }
+    private void reportWithLegacy(JSONObject jsonReport) {
+        String jsonReportString = jsonReport.toString();
+        SubmitReportLegacyService.start(mContext, jsonReportString);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void reportWithJobScheduler(Report report) {
+    private void reportWithJobScheduler(JSONObject jsonReport) {
         JobScheduler jobScheduler = (JobScheduler) mContext.getSystemService(Context.JOB_SCHEDULER_SERVICE);
         if (jobScheduler == null) {
             throw new RuntimeException("Failed to obtain JobScheduler!");
         }
 
-        try {
-            int jobId = (int) System.currentTimeMillis();
-            String jsonReport = report.toJSON().toString();
+        int jobId = (int) System.currentTimeMillis();
+        String jsonReportString = jsonReport.toString();
 
-            PersistableBundle data = new PersistableBundle();
-            data.putString(SubmitReportService.KEY_DATA_PAYLOAD, jsonReport);
+        PersistableBundle data = new PersistableBundle();
+        data.putString(SubmitReportService.KEY_DATA_PAYLOAD, jsonReportString);
 
-            JobInfo info = new JobInfo.Builder(jobId, SubmitReportService.getComponentName(mContext))
-                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                    .setPersisted(true)
-                    .setExtras(data)
-                    .build();
+        JobInfo info = new JobInfo.Builder(jobId, SubmitReportService.getComponentName(mContext))
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPersisted(true)
+                .setExtras(data)
+                .build();
 
-            jobScheduler.schedule(info);
-        } catch (JSONException e) {
-            Log.e("Error serializing JSON report", e);
-        }
+        jobScheduler.schedule(info);
     }
 }
